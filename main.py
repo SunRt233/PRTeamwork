2d6f00c0782d54e82834d88709f3ee7692b084ff
SunRt233 <32709388+SunRt233@users.noreply.github.com>
2024-12-09 19:08:26 +0800

    feat: 添加训练和展示模式- 新增 main.py 文件，实现训练和展示模式的功能 - 添加 TrainSave 类，用于保存训练结果和评估指标 - 实现数据加载、模型训练、结果保存和展示等功能 - 支持 LDA 和 PCA 两种降维方法 - 提供交互式和非交互式两种运行方式

---
+++ main.py
@@ -0,0 +1,223 @@
+import argparse
+import os
+import re
+from datetime import datetime
+
+from prettytable import PrettyTable
+
+from classifier import PCAKNNClassifier, LDAKNNClassifier, BaseClassifier
+from data import DataProvider
+from save import TrainSave
+
+saves = r'./saves'
+
+running_modes = [
+    {'编号': 1, '模式': '训练', '描述': '批量运行调整超参数'},
+    {'编号': 2, '模式': '展示', '描述': '加载历史最优并展示'}
+]
+
+support_methods = [
+    {'编号': 1, '方法': 'LDA', '描述': 'LDA降维+KNN分类'},
+    {'编号': 2, '方法': 'PCA', '描述': 'PCA降维+KNN分类'}
+]
+
+
+def mode_train(interactive: bool):
+    def train(classifier: BaseClassifier, data_provider: DataProvider, k_min, k_max, shuffle=False):
+        train_data, verification_data, test_data = (
+            data_provider.provide_balanced_split_data(train_ratio=0.6,
+                                                      verification_ratio=0.2,
+                                                      shuffle=shuffle))
+        save = TrainSave(classifier.__class__.__name__, train_data, verification_data, test_data)
+        for k in range(k_min, k_max + 1):
+            classifier.train(train_data, k=k)
+            result, probability = classifier.predict(verification_data.samples)
+            save.add_evaluation_result(k, result, probability)
+        return save
+
+    data_provider = DataProvider(r'./原始数据/S3-CTG.xlsx')
+    data_provider.load_data()
+
+    classifiers: list[BaseClassifier] = []
+
+    table = PrettyTable()
+    table.field_names = ["编号", "方法", "描述"]
+    for item in support_methods:
+        table.add_row([item['编号'], item['方法'], item['描述']])
+
+    while interactive:
+        print(table)
+        match input(prompt('选择要使用的方法（输入all表示使用所有方法）', suffix='：')):
+            case '1':
+                classifiers.append(LDAKNNClassifier())
+                break
+            case '2':
+                classifiers.append(PCAKNNClassifier())
+                break
+            case 'all':
+                break
+            case _:
+                print('请输入正确的方法编号！')
+                continue
+
+    if not interactive or len(classifiers) == 0:
+        print(table)
+        print('已选择所有方法')
+        classifiers.append(PCAKNNClassifier())
+        classifiers.append(LDAKNNClassifier())
+
+    # 输入超参数 K 的搜索范围，默认最小值为5，默认最大值为25
+    k_min = 5
+    k_max = 25
+    while interactive:
+        input_str = input(prompt(f'输入 K 的最小值，默认为 {k_min}', suffix='：'))
+        match input_str:
+            case '':
+                break
+            case _:
+                try:
+                    k_min = int(input_str)
+                    if k_min < 5:
+                        print('K 的最小值不能小于5！')
+                        continue
+                    break
+                except ValueError:
+                    print('请输入正确的整数！')
+    while interactive:
+        input_str = input(prompt(f'输入 K 的最大值，默认为 {k_max}', suffix='：'))
+        match input_str:
+            case '':
+                break
+            case _:
+                try:
+                    k_max = int(input_str)
+                    if k_max < k_min:
+                        print('K 的最大值不能小于K的最小值！')
+                        continue
+                    break
+                except ValueError:
+                    print('请输入正确的整数！')
+
+    if not interactive:
+        # 打印K默认范围
+        print(f'K 的默认范围是 {k_min} ~ {k_max}')
+
+    shuffle_turn = 1
+    while interactive:
+        input_str = input(prompt(f'输入打乱次数，默认为 {shuffle_turn}', suffix='：'))
+        match input_str:
+            case '':
+                break
+            case _:
+                try:
+                    shuffle_turn = int(input_str)
+                    if shuffle_turn < 0:
+                        print('打乱次数不能小于0！')
+                        continue
+                    break
+                except ValueError:
+                    print('请输入正确的整数！')
+    if not interactive:
+        print(f'打乱次数默认为 {shuffle_turn}')
+
+    train_saves = []
+    print('开始训练')
+
+    time = datetime.now().strftime("[%Y-%m-%d_%H_%M_%S]")
+    for t in range(1, shuffle_turn + 1):
+        for classifier in classifiers:
+            print(f'开始训练 {classifier.__class__.__name__}，第 {t} 次打乱')
+            train_save = train(classifier, data_provider, k_min, k_max, shuffle=True)
+            train_save.save(saves, f'{time}{classifier.__class__.__name__}', t)
+            train_saves.append(train_save)
+
+    print('训练完成')
+    # Table打印summaries
+    table = PrettyTable()
+    table.field_names = ['分类器', '训练集大小', '验证集大小', '测试集大小', 'K值范围', '最优K值', '最差K值',
+                         '中位数K值', '最优au_roc', '最差au_roc', '中位数au_roc']
+    for train_save in train_saves:
+        summary = train_save.summary()
+        table.add_row(
+            [summary['分类器'], summary['训练集大小'], summary['验证集大小'], summary['测试集大小'], summary['K值范围'],
+             summary['最优K值'], summary['最差K值'], summary['中位数K值'], summary['最优au_roc'], summary['最差au_roc'],summary['中位数au_roc']])
+    print(table)
+
+
+def mode_show():
+    # Table列出saves下的TrainSave文件
+    table = PrettyTable()
+    table.field_names = ['编号', '时间', '分类器', '最大打乱轮数']
+
+    # 定义正则表达式
+    pattern = re.compile(r'^\[(?P<time>[^\]]+)\](?P<classifier>[^.]+)\.summary(?:\.(?P<shuffle>\d+))?\.csv$')
+
+    # 使用字典存储每个时间-分类器组合的最大打乱轮数
+    max_shuffle_dict = {}
+
+    for file in os.listdir(saves):
+        match = pattern.match(file)
+        if match:
+            # 提取时间
+            time = match.group('time')
+            # 提取分类器
+            classifier = match.group('classifier')
+            # 提取打乱次数（如果有）
+            shuffle = int(match.group('shuffle')) if match.group('shuffle') else 0
+
+            # 更新最大打乱轮数
+            key = (time, classifier)
+            if key in max_shuffle_dict:
+                max_shuffle_dict[key] = max(max_shuffle_dict[key], shuffle)
+            else:
+                max_shuffle_dict[key] = shuffle
+
+    # 将字典中的信息添加到表格中
+    i = 1
+    for (time, classifier), max_shuffle in max_shuffle_dict.items():
+        table.add_row([i, time, classifier, max_shuffle if max_shuffle > 0 else '无'])
+        i += 1
+
+    print(table)
+
+
+def prompt(str, prefix='', suffix=' >'):
+    if len(prefix) > 0:
+        prefix = f'({prefix}) '
+    return f'{prefix}{str}{suffix} '
+
+
+def main(args):
+    # 选择运行模式，要验证输入是否合法
+    table = PrettyTable()
+    table.field_names = ["编号", "模式", "描述"]
+
+    # 添加行数据
+    for item in running_modes:
+        table.add_row([item['编号'], item['模式'], item['描述']])
+
+    os.makedirs(saves, exist_ok=True)
+
+    while args.isPresenting:
+        # 打印表格
+        print(table)
+        mode = input(prompt('选择运行模式', suffix='：'))
+        match mode:
+            case '1':
+                mode_train(interactive=True)
+                break
+            case '2':
+                mode_show()
+                break
+            case _:
+                print('请输入正确的模式编号！')
+    if not args.isPresenting:
+        print('当前非演示模式')
+        mode_train(interactive=False)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--isPresenting', action='store_true', help='是否展示运行模式', default=False)
+    args = parser.parse_args()
+    main(args)
---
+++ save.py
@@ -0,0 +1,82 @@
+import pandas as pd
+from sklearn.metrics import roc_auc_score
+
+from classifier import BaseClassifier
+from data import DataContainer
+
+
+class TrainSave:
+    summary_keys = ['分类器', '训练集大小', '验证集大小', '测试集大小', '最优K值','验证集AU-ROC']
+
+    def __init__(self, classifier: str, train_data: DataContainer, verification_data: DataContainer,
+                 test_data: DataContainer):
+        self.classifier = classifier
+        self.train_data = train_data
+        self.verification_data = verification_data
+        self.test_data = test_data
+        self.evaluation_results = {}
+        self.au_roc = {}
+
+        col = [f'特征{i}' for i in range(self.train_data.num_features())]
+        col.extend(['标签'])
+
+        tr_d = [[*self.train_data.samples[i], self.train_data.labels[i]] for i in range(self.train_data.num_samples())]
+        tr_df = pd.DataFrame(columns=col, data=tr_d)
+        ve_d = [[*self.verification_data.samples[i], self.verification_data.labels[i]] for i in
+                range(self.verification_data.num_samples())]
+        ve_df = pd.DataFrame(columns=col, data=ve_d)
+        te_d = [[*self.test_data.samples[i], self.test_data.labels[i]] for i in range(self.test_data.num_samples())]
+        te_df = pd.DataFrame(columns=col, data=te_d)
+
+        self.dataset_dfs = {'train': tr_df, 'verification': ve_df, 'test': te_df}
+
+
+    def add_evaluation_result(self, k, predict_result, predict_probability):
+        au_roc = roc_auc_score(y_true=self.verification_data.labels, y_score=predict_probability, multi_class='ovo',average='weighted',labels=self.verification_data.classes())
+        self.au_roc[k] = au_roc
+        self.evaluation_results[k] = [[self.verification_data.labels[i], predict_result[i], *predict_probability[i]] for
+                                      i in range(self.verification_data.num_samples())]
+
+    def save(self, path, name, shuffle_turns = None | int):
+        suffix = 'csv'
+        if shuffle_turns is not None:
+            suffix = f'{shuffle_turns}.csv'
+
+        # 用pandas保存为CSV
+        # 训练集，验证集，测试集保存为三个CSV
+        for key, df in self.dataset_dfs.items():
+            df.to_csv(f'{path}/{name}.{key}.{suffix}', index=False)
+
+        # 保存评估结果
+        data_to_save = []
+        for k in self.evaluation_results.keys():
+            for result in self.evaluation_results[k]:
+                data_to_save.append([k,*result])
+        col = ['K','真实标签','预测标签',*[f'预测为{i+1}的概率' for i in range(self.train_data.num_classes())]]
+        pd.DataFrame(data_to_save, columns=col).to_csv(f'{path}/{name}.evaluation.{suffix}', index=False)
+
+        au_roc_data = []
+        for k in self.au_roc.keys():
+            au_roc_data.append([k,self.au_roc[k]])
+        col = ['K','验证集AU-ROC']
+        pd.DataFrame(au_roc_data, columns=col).to_csv(f'{path}/{name}.au_roc.{suffix}', index=False)
+
+        # 保存summary()返回的字典到csv
+        pd.DataFrame([self.summary()]).to_csv(f'{path}/{name}.summary.{suffix}',index=False)
+
+    def summary(self) -> dict:
+        # 返回一个字典
+        return {
+            '分类器': self.classifier,
+            '训练集大小': self.train_data.num_samples(),
+            '验证集大小': self.verification_data.num_samples(),
+            '测试集大小': self.test_data.num_samples(),
+            'K值范围': f'{min(self.evaluation_results.keys())}~{max(self.evaluation_results.keys())}',
+            # au_roc的键
+            '最优K值': max(self.au_roc, key=self.au_roc.get),
+            '最差K值': min(self.au_roc, key=self.au_roc.get),
+            '中位数K值': sorted(self.au_roc.keys())[len(self.au_roc) // 2],
+            '最优au_roc': max(self.au_roc.values()),
+            '最差au_roc': min(self.au_roc.values()),
+            '中位数au_roc': sorted(self.au_roc.values())[len(self.au_roc) // 2]
+        }
